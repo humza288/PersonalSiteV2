eb9cf1eead292ee78e8c77c4c4d952583f72b5aa
Humza Munir <humza@Humzas-MacBook-Air.local>
2020-09-26 04:10:27 -0700

    fixed up project section

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,17 +1,8 @@
 const cards = document.querySelectorAll('.card');
 const nav = document.querySelector('.nav');
 const forms = document.querySelectorAll('.contact-form');
-    
-let observer = new IntersectionObserver((enteries) => {
-    enteries.forEach(entry => {
-        if(entry.intersectionRatio > 0) {
-            console.log("Hi")
-            entry.target.style.opacity = '1';  
-        }
-    })
-})
 
-let observerTranslate = new IntersectionObserver((enteries) => {
+let observer = new IntersectionObserver((enteries) => {
     enteries.forEach(entry => {
         if(entry.intersectionRatio > 0) {
             entry.target.style.transform = "translateY(0px)"
@@ -21,7 +12,7 @@ let observerTranslate = new IntersectionObserver((enteries) => {
 })
 
 cards.forEach(card => {
-    observerTranslate.observe(card);
+    observer.observe(card);
 })
 
 forms.forEach(form => {
